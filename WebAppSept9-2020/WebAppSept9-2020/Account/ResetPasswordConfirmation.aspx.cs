c78a72ff5f99e60688fafcd324c0a876e15ccfd2
PavneetR <pavneetrai@hotmail.com>
2020-09-09 07:41:42 -0700

    create skeleton solution with web app and library

---
+++ WebAppSept9-2020/WebAppSept9-2020/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebAppSept9_2020.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebAppSept9-2020/WebAppSept9-2020/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppSept9_2020.Startup))]
+namespace WebAppSept9_2020
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
